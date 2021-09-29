ecbde9e9aa5709e5c8754ff429d39744dfe4fc99
tktnskan <jny4594@gmail.com>
2021-09-29 16:55:59 +0900

    first commit

---
+++ GitHubSearch/GitHubSearch/FavoriteListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteListVC.swift
+//  GitHubSearch
+//
+//  Created by GJC03280 on 2021/09/28.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
---
+++ GitHubSearch/GitHubSearch/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubSearch
+//
+//  Created by GJC03280 on 2021/09/28.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
