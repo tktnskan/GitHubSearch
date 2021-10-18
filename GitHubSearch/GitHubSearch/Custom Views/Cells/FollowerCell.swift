ee92af2f1d89852179ca9720c43c17dcb4ba1cb3
tktnskan <jny4594@gmail.com>
2021-10-18 12:32:59 +0900

    follower cell 추가

---
+++ GitHubSearch/GitHubSearch/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,51 @@
+//
+//  FollowerCell.swift
+//  GitHubSearch
+//
+//  Created by GJC03280 on 2021/10/18.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    private let padding: CGFloat = 8
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureImage()
+        configureLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configureImage() {
+        addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+        ])
+    }
+    
+    private func configureLabel() {
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20),
+        ])
+    }
+}
--- GitHubSearch/GitHubSearch/VC/FollowerListVC.swift
+++ GitHubSearch/GitHubSearch/VC/FollowerListVC.swift
@@ -10,25 +10,41 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var username: String!
-
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad", message: error!, buttonTitle: "Ok")
-                return
-            }
-            
-            print("Followers.count = \(followers.count)")
-            print(followers)
-        }
+        configureCollectionView()
+        configureVC()
+        getFollowers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureVC() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
