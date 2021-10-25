21db08d4e5de13684bef7d2af00e1e6aaa872df7
tktnskan <jny4594@gmail.com>
2021-10-25 16:53:43 +0900

    searchVC padding값 수정

--- GitHubSearch/GitHubSearch/VC/SearchVC.swift
+++ GitHubSearch/GitHubSearch/VC/SearchVC.swift
@@ -57,7 +57,7 @@ class SearchVC: UIViewController {
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.image = Images.ghLogo
         
-        let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 20 : 0
+        let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 20 : 80
         
         logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
         logoImageViewTopConstraint.isActive = true
